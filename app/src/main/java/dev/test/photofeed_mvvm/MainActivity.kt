c8ecc25e4fe5d82f14a5a258048ebf7daaf3da84
Arnault <arnault.pascual.webmaster@gmail.com>
2022-12-12 09:48:24 +0100

    add databinding and entrypoint for DI

--- app/src/main/java/dev/test/photofeed_mvvm/MainActivity.kt
+++ app/src/main/java/dev/test/photofeed_mvvm/MainActivity.kt
@@ -2,10 +2,17 @@ package dev.test.photofeed_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import dev.test.photofeed_mvvm.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
